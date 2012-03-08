a25c2a57b387ef52127e6877a8402279e5d363bd
Kevin G. Fourie <kevin@4e.co.za>
2012-03-08 16:58:34 +0200

    Added missing database config section.

--- config/application.rb
+++ config/application.rb
@@ -25,4 +25,11 @@ EM.error_handler do |e|
   logger.error "Error raised during asynchronous event loop: #{e.message}"
 end
 
+# Database
+db_settings = YAML.load_file(Api.root_path.join('config/database.yml'))[config[:env]]
+config['db'] = EventMachine::Synchrony::ConnectionPool.new(:size => 20) do
+  conn = EM::Mongo::Connection.new(db_settings['host'], db_settings['port'], 1, {:reconnect_in => 1})
+  conn.db(db_settings['database'])
+end
+
 Api.configure(self)
