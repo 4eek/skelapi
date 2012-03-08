a6b8a1081f5f279e39d05fdfa5e8d343c638afc8
Kevin G. Fourie <kevin@4e.co.za>
2012-03-08 16:43:27 +0200

    Whitespace removals.

--- lib/api/api_status.rb
+++ lib/api/api_status.rb
@@ -1,18 +1,18 @@
 module Api
   class ApiStatus
     ENDPOINT = '/status'
-    
+
     attr_accessor :env, :params
-    
+
     def initialize(env, params)
       self.env      = env
       self.params   = params
     end
-    
+
     def get
       response = {:env => Goliath.env}
       [200, {}, response]
     end
-    
+
   end
 end
--- lib/api/router.rb
+++ lib/api/router.rb
@@ -2,12 +2,12 @@ module Api
   class Router < Goliath::API
     use ::Rack::ContentLength
     use Goliath::Rack::Heartbeat, { :path => '/ping', :response => [200, {}, []] }
-    
+
     post ApiStatus::ENDPOINT, ApiStatusController
-    
+
     not_found do
       run PageNotFoundController.new
     end
-    
+
   end
 end
