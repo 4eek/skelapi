9fa523b270c6f31062f1dbda79f9634df6fb1139
Kevin G. Fourie <kevin@4e.co.za>
2012-04-19 17:50:10 +0200

    Some exception handling.

--- lib/api/models/api_status.rb
+++ lib/api/models/api_status.rb
@@ -10,15 +10,28 @@ module Api
     end
 
     def method_router
-      send("#{params['method']}_method".to_sym, params) rescue raise "Method #{params['method']||'EMPTY_METHOD'} not found."
+      begin
+        send("#{params['method']}_method".to_sym, params)
+      rescue ApiError
+        raise
+      rescue
+        raise "Method #{params['method']||'EMPTY_METHOD'} not found."
+      end
     end
 
     def environment_method(params)
-      env.trace('method_beg')
-      EM::Synchrony.sleep(5) # pretent it's a lot of work
-      response = {:env => Goliath.env, :method => params['method']}
-      env.trace('method_end')
-      [200, {}, response]
+      begin
+        env.trace('method_beg')
+        # raise Goliath::Validation::Error.new(401, "Invalid param") unless params['some_param'] and params['some_param'] != ''
+        EM::Synchrony.sleep(5) # pretent it's a lot of work
+        response = {:env => Goliath.env, :method => params['method']}
+        env.trace('method_end')
+        [200, {}, response]
+      rescue Goliath::Validation::Error => e
+        raise ApiError, "API Validation: #{e.message}"
+      rescue => e
+        raise ApiError, "API Exception: MESSAGE:#{e.message} BACKTRACE:#{e.backtrace.join('____')}"
+      end
     end
 
   end
